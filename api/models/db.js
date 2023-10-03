26b450a31aff8ef514b70f531f4edc92f98bdc6b
Papiricoh <slendeman0200@gmail.com>
2023-10-03 21:54:03 +0200

    delete some error documentation

--- api/models/db.js
+++ api/models/db.js
@@ -1,7 +1,6 @@
 const mysql = require("mysql2");
 const dbConfig = require("../config/config.js");
 
-// Create a connection to the database
 const connection = mysql.createConnection({
     host: dbConfig.HOST,
     user: dbConfig.USER,
@@ -9,7 +8,6 @@ const connection = mysql.createConnection({
     database: dbConfig.DB
 });
 
-// open the MySQL connection
 connection.connect(error => {
     if (error) throw error;
     console.log("Successfully connected to the database.");
