5fe34e48813646fe779b9cc0cb056bf9433d8523
papiricoh <slendeman0200@gmail.com>
2024-02-21 02:56:53 +0100

    added file storage

--- api.js
+++ api.js
@@ -4,6 +4,42 @@ const bodyParser = require('body-parser');
 const app = express();
 const routes = require('./api/routes');
 
+//Sistema de almacenaje de archivos
+const multer = require('multer');
+const path = require('path');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+const upload = multer({ storage: storage });
+
+app.post('/upload', upload.single('imagen'), (req, res) => {
+    const file = req.file;
+    if (!file) {
+        return res.status(400).send('No se ha subido ningún archivo.');
+    }
+
+    const imageUrl = `${req.protocol}://${req.get('host')}/uploads/${file.filename}`;
+
+    //Almacenar la URL en tu base de datos
+
+    // Envía la URL de la imagen como respuesta
+    res.status(200).json({
+        message: 'Archivo subido con éxito',
+        imageUrl: imageUrl
+    });
+});
+
+app.use('/uploads', express.static('uploads'));
+
+//Fin del sistema de almacenaje de archivos
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
--- api/routes/index.js
+++ api/routes/index.js
@@ -3,19 +3,6 @@ const router = express.Router();
 const mainController = require('../controllers/mainController.js');
 const userController = require('../controllers/userController.js');
 const fileController = require('../controllers/fileController.js');
-const multer = require('multer');
-const path = require('path');
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, './uploads/');
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
-
-const upload = multer({ storage: storage });
 
 /*
 #############################################################
